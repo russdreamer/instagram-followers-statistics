2249093563da8a300b4026186699e99c3a56296d
russdreamer <russdreamer@gmail.com>
2020-08-31 01:51:47 +0300

    add app switcher

--- console_script.js
+++ console_script.js
@@ -6,10 +6,12 @@ let statistics;
 let loader_wrapper;
 let action_panel;
 let errorDiv;
-let	errortitle 
+let	errortitle;
 let textFile = null;
 let isFirstLaunch = true;
 let currentAction;
+let cssDiv;
+let switchDiv;
 
 const actionType = {
     FOLLOW:"FOLLOW",
@@ -31,13 +33,36 @@ class Action {
 
 (function () {
 	try {
-		document.body.setAttribute("style", "display: none");
-  		loadCSS();
-  		start();
+		createSwitcher();
 	} catch (e) {
   		showError(e);
 	}
 })();
+
+function createSwitcher() {
+	switchDiv = document.createElement("DIV");
+	switchDiv.setAttribute("style", "position: fixed;z-index: 5;margin-top: 54px;");
+	const switchBtn = document.createElement("BUTTON");
+	switchBtn.setAttribute("style", "background-color: #0095f6;color: white;border-radius: 4px;border-width: 0px;padding: 5px;font-weight: bold;margin: 0;cursor: pointer;");
+	switchBtn.textContent = "Open app";
+	switchBtn.addEventListener('click', () => manageSwitcher(switchBtn));
+	switchDiv.appendChild(switchBtn);
+	document.documentElement.prepend(switchDiv);
+}
+
+function manageSwitcher(switchBtn) {
+	if (root == null || root.parentNode == null) {
+		document.body.setAttribute("style", "display: none");
+  		loadCSS();
+  		start();
+  		switchBtn.textContent = "Close app";
+	} else {
+		document.body.style.display="";
+		root.remove();
+		cssDiv.remove();
+		switchBtn.textContent = "Open app";
+	}
+}
 
 function showError(error) {
 	errorDiv.style.display = "grid";
@@ -76,9 +101,10 @@ function readSingleFile(e) {
 }
 
 function loadCSS() {
-	const css = '<style>#root {display: flex;align-items: center;} #content {padding: 20px}#script_root {background: teal;overflow: hidden;border-radius: 25px;border: 2px solid #35c7ac;width: fit-content;max-width: 70vw;height: fit-content;}.button_round {width: fit-content;margin-top:10px;margin-bottom: 20px;box-shadow: 3px 4px 0px 0px #899599;background:linear-gradient(to bottom, #ededed 5%, #bab1ba 100%);background-color:#ededed;border-radius:15px;border:1px solid #d6bcd6;display:inline-block;cursor:pointer;color:#0b4c5c;font-family:Arial;font-size:17px;padding:7px 25px;text-decoration:none;text-shadow:0px 1px 0px #e1e2ed;}.button_round:hover {background:linear-gradient(to bottom, #bab1ba 5%, #ededed 100%);background-color:#bab1ba;}.button_round:active {position:relative;top:1px;}.tab {overflow-x: auto;white-space: nowrap;display: block;border: 1px solid #ccc;background-color: #f1f1f1;}.tab button {background-color: inherit;border: none;outline: none;cursor: pointer;padding: 14px 16px;transition: 0.3s;font-size: 17px;}.tab button:hover {background-color: #ddd;}.tab button.active {background-color: #ccc;}.tabcontent {background: white;overflow-y: scroll;max-height: 500px;display: none;padding: 6px 12px;border: 1px solid #ccc;border-top: none;}.sub_tabcontent {background: white;overflow-y: scroll;max-height: 500px;display: none;padding: 6px 12px;border: 1px solid #ccc;border-top: none;}.avatar {width: 60px;float:left}.user_content {display:table;padding:10px;font-size:15pt}.username {color: darkcyan}.user_row {width: fit-content; margin-top: 10px;display: block; cursor: pointer}.info {color: white;padding-top: 30px;font-weight: bold;font-size: 15pt;}.table_content {border-radius: 25px;overflow: hidden;width:fit-content;max-width:100%; margin-top:10px;}.load_container{display:none; position: absolute;align-items: center;justify-content: center;height: 100%;width: 100%;background-color: rgba(0, 0, 0, 0.8)}.loader {border: 16px solid #f3f3f3;border-radius: 50%;border-top: 16px solid #3498db;width: 120px;height: 120px;-webkit-animation: spin 2s linear infinite; /* Safari */animation: spin 1s linear infinite;}@-webkit-keyframes spin {0% { -webkit-transform: rotate(0deg); }100% { -webkit-transform: rotate(360deg); }}@keyframes spin {0% { transform: rotate(0deg); }100% { transform: rotate(360deg);}} .action_panel {position: absolute;overflow-y: scroll;align-self: center;background: teal;width: 100%;height: 100%} .progress_number{color: white;position: absolute;font-weight: bold;}.tooltip {position: relative;display: inline-block;border-bottom: 1px dotted black;}.tooltip .tooltiptext {visibility: hidden;width: 200px;background-color: #555;color: #fff;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;bottom: 125%;left: 50%;margin-left: -60px;opacity: 0;transition: opacity 0.3s;}.tooltip .tooltiptext::after {content: "";position: absolute;top: 100%;left: 50%;margin-left: -5px;border-width: 5px;border-style: solid;border-color: #555 transparent transparent transparent;}.tooltip:hover .tooltiptext {visibility: visible;opacity: 1;}</style>';
-	const css2 = '.tooltip {position: relative;display: inline-block;border-bottom: 1px dotted black;}.tooltip .tooltiptext {visibility: hidden;width: 200px;background-color: #555;color: #fff;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;bottom: 125%;left: 50%;margin-left: -60px;opacity: 0;transition: opacity 0.3s;}.tooltip .tooltiptext::after {content: "";position: absolute;top: 100%;left: 50%;margin-left: -5px;border-width: 5px;border-style: solid;border-color: #555 transparent transparent transparent;}.tooltip:hover .tooltiptext {visibility: visible;opacity: 1;}';
-	document.head.innerHTML += css;
+	cssDiv = document.createElement('STYLE');
+	const css = '#root {display: flex;align-items: center;} #content {padding: 20px}#script_root {background: teal;overflow: hidden;border-radius: 25px;border: 2px solid #35c7ac;width: fit-content;max-width: 70vw;height: fit-content;}.button_round {width: fit-content;margin-top:10px;margin-bottom: 20px;box-shadow: 3px 4px 0px 0px #899599;background:linear-gradient(to bottom, #ededed 5%, #bab1ba 100%);background-color:#ededed;border-radius:15px;border:1px solid #d6bcd6;display:inline-block;cursor:pointer;color:#0b4c5c;font-family:Arial;font-size:17px;padding:7px 25px;text-decoration:none;text-shadow:0px 1px 0px #e1e2ed;}.button_round:hover {background:linear-gradient(to bottom, #bab1ba 5%, #ededed 100%);background-color:#bab1ba;}.button_round:active {position:relative;top:1px;}.tab {overflow-x: auto;white-space: nowrap;display: block;border: 1px solid #ccc;background-color: #f1f1f1;}.tab button {background-color: inherit;border: none;outline: none;cursor: pointer;padding: 14px 16px;transition: 0.3s;font-size: 17px;}.tab button:hover {background-color: #ddd;}.tab button.active {background-color: #ccc;}.tabcontent {background: white;overflow-y: scroll;max-height: 500px;display: none;padding: 6px 12px;border: 1px solid #ccc;border-top: none;}.sub_tabcontent {background: white;overflow-y: scroll;max-height: 500px;display: none;padding: 6px 12px;border: 1px solid #ccc;border-top: none;}.avatar {width: 60px;float:left}.user_content {display:table;padding:10px;font-size:15pt}.username {color: darkcyan}.user_row {width: fit-content; margin-top: 10px;display: block; cursor: pointer}.info {color: white;padding-top: 30px;font-weight: bold;font-size: 15pt;}.table_content {border-radius: 25px;overflow: hidden;width:fit-content;max-width:100%; margin-top:10px;}.load_container{display:none; position: absolute;align-items: center;justify-content: center;height: 100%;width: 100%;background-color: rgba(0, 0, 0, 0.8)}.loader {border: 16px solid #f3f3f3;border-radius: 50%;border-top: 16px solid #3498db;width: 120px;height: 120px;-webkit-animation: spin 2s linear infinite; /* Safari */animation: spin 1s linear infinite;}@-webkit-keyframes spin {0% { -webkit-transform: rotate(0deg); }100% { -webkit-transform: rotate(360deg); }}@keyframes spin {0% { transform: rotate(0deg); }100% { transform: rotate(360deg);}} .action_panel {position: absolute;overflow-y: scroll;align-self: center;background: teal;width: 100%;height: 100%} .progress_number{color: white;position: absolute;font-weight: bold;}.tooltip {position: relative;display: inline-block;border-bottom: 1px dotted black;}.tooltip .tooltiptext {visibility: hidden;width: 200px;background-color: #555;color: #fff;text-align: center;border-radius: 6px;padding: 5px 0;position: absolute;z-index: 1;bottom: 125%;left: 50%;margin-left: -60px;opacity: 0;transition: opacity 0.3s;}.tooltip .tooltiptext::after {content: "";position: absolute;top: 100%;left: 50%;margin-left: -5px;border-width: 5px;border-style: solid;border-color: #555 transparent transparent transparent;}.tooltip:hover .tooltiptext {visibility: visible;opacity: 1;}';
+	cssDiv.innerHTML = css;
+	document.head.appendChild(cssDiv);
 }
 
 function getMutual(followersMap, followingsMap) {
@@ -234,7 +260,7 @@ function openActionPanel(actionType) {
 }
 
 function closeActionPanel() {
-	content.removeChild(action_panel);
+	action_panel.remove();
 	errorDiv.style.display = "none";
 }
 
@@ -686,7 +712,7 @@ function addToMap(map, edges) {
 }
 
 function restart() {
-	document.documentElement.removeChild(root);
+	root.remove();
 	start();
 }
 
@@ -721,7 +747,7 @@ function showStatisctic() {
 
 function showMassActionsPanel() {
 	showLoader();
-	content.removeChild(intro);
+	intro.remove();
 	const massActionPanel = generateMassActionsPanel();
 	content.appendChild(massActionPanel);
 	hideLoader();
@@ -735,7 +761,7 @@ function generateFirstList() {
 	const followings = getFollowings(action);
 	Promise.all([followers, followings]).then(values => {
 		if (!action.isAborted) {
-			content.removeChild(intro);
+			intro.remove();
 			const statistics = showStatisctic(); 
 			statistics.appendChild(getDownloadBtn(new Statistics(values[0])));
 			statistics.appendChild(makeTable(...values));
@@ -771,7 +797,7 @@ function generateNextList(stat) {
 	Promise.all([followers, followings]).then(values => {
 		if (!action.isAborted) {
 			updateStat(stat, values[0]);
-			content.removeChild(intro);
+			intro.remove();
 			const statistics = showStatisctic(); 
 			statistics.appendChild(getDownloadBtn(stat));
 			statistics.appendChild(makeTable(...values));
@@ -897,7 +923,7 @@ function start() {
 	root.appendChild(scriptRoot);
 	root.appendChild(errorDiv);
 	const doc = document.documentElement
-	doc.prepend(root);
+	switchDiv.after(root);
 	document.getElementById('file-input')
 	.addEventListener('change', readSingleFile, false);
 	scriptRoot.appendChild(createLoader());
